43dba9e7d68f3a3f5d86140b4861affcffdbea1e
Tonny Kohar <13114803+tonny-kohar@users.noreply.github.com>
2020-05-10 17:22:41 +0700

    code cleanup for alkitab 4.0

--- src/kiyut/alkitab/modules/downloader/actions/DownloaderAction.java
+++ src/kiyut/alkitab/modules/downloader/actions/DownloaderAction.java
@@ -15,6 +15,10 @@ import org.crosswire.jsword.util.WebWarning;
 import org.openide.DialogDisplayer;
 import org.openide.LifecycleManager;
 import org.openide.NotifyDescriptor;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionRegistration;
 import org.openide.util.HelpCtx;
 import org.openide.util.NbBundle;
 import org.openide.util.actions.CallableSystemAction;
@@ -24,7 +28,13 @@ import org.openide.windows.WindowManager;
  * Action that will open downloader panel.
  * It delegates into org.crosswire.bibledesktop.book.install.SitesPane
  * 
- */
+ * @author Tonny Kohar <tonny.kohar@gmail.com>
+  */
+@ActionID(id = "kiyut.alkitab.modules.downloader.actions.DownloaderAction", category = "Tools")
+@ActionRegistration(displayName = "#CTL_DownloaderAction.Text", lazy = false)
+@ActionReferences({
+    @ActionReference(path = "Menu/Tools", position = 10)
+})
 public class DownloaderAction extends CallableSystemAction {
 
     private boolean booksChanged;
@@ -47,52 +57,46 @@ public class DownloaderAction extends CallableSystemAction {
     
     @Override
     public void performAction() {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                if (WebWarning.instance().isShown()) {
-                    int choide = InternetWarning.showDialog(WindowManager.getDefault().getMainWindow(), "?");
-                    if (choide != InternetWarning.GRANTED) {
-                        return;
-                    }
+        SwingUtilities.invokeLater(() -> {
+            if (WebWarning.instance().isShown()) {
+                int choide = InternetWarning.showDialog(WindowManager.getDefault().getMainWindow(), "?");
+                if (choide != InternetWarning.GRANTED) {
+                    return;
+                }
+            }
+            
+            SitesPane sitesPane = new SitesPane();
+            sitesPane.showInDialog(WindowManager.getDefault().getMainWindow());
+            
+            // XXX below is hack, should ask JSword Dev for proper API
+            
+            Window window = SwingUtilities.getWindowAncestor(sitesPane);
+            if (window == null) { return; }
+            
+            window.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowOpened(WindowEvent evt) {
+                    Books.installed().addBooksListener(booksListener);
                 }
                 
-                SitesPane sitesPane = new SitesPane();
-                sitesPane.showInDialog(WindowManager.getDefault().getMainWindow());
-                
-                // XXX below is hack, should ask JSword Dev for proper API
-                
-                Window window = SwingUtilities.getWindowAncestor(sitesPane);
-                if (window == null) { return; }
-                
-                window.addWindowListener(new WindowAdapter() {
-                    @Override
-                    public void windowOpened(WindowEvent evt) {
-                        Books.installed().addBooksListener(booksListener);
+                /*@Override
+                public void windowClosing(WindowEvent evt) {
+                // why this is not called
+                System.out.println("WindowClosing called: " + booksChanged);
+                }*/
+            });
+            
+            window.addComponentListener(new ComponentAdapter() {
+                @Override
+                public void componentHidden(ComponentEvent evt) {
+                    Books.installed().removeBooksListener(booksListener);
+                    if (booksChanged) {
+                        SwingUtilities.invokeLater(() -> {
+                            confirmRestart();
+                        });
                     }
-
-                    /*@Override
-                    public void windowClosing(WindowEvent evt) {
-                        // why this is not called
-                        System.out.println("WindowClosing called: " + booksChanged);
-                    }*/
-                }); 
-                
-                window.addComponentListener(new ComponentAdapter() {
-                    @Override
-                    public void componentHidden(ComponentEvent evt) {
-                        Books.installed().removeBooksListener(booksListener);
-                        if (booksChanged) {
-                            SwingUtilities.invokeLater(new Runnable() {
-                                @Override
-                                public void run() {
-                                    confirmRestart();
-                                }
-                            });
-                        }
-                    }
-                });
-            } 
+                }
+            }); 
         });
     }
 
